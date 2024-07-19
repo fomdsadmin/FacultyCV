cecdcae63d108f1a464c2929e90564259384c6ae
abhiverma13 <a.verma1304@gmail.com>
2024-07-19 16:06:51 -0700

    completed assistant page functionality

--- frontend/src/Components/AssociatedUser.jsx
+++ frontend/src/Components/AssociatedUser.jsx
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { TiDelete } from "react-icons/ti";
+import { updateUserConnection, deleteUserConnection } from '../graphql/graphqlHelpers.js';
 
-const AssociatedUser = ({ assistant }) => {
+
+const AssociatedUser = ({ connection, getAllUserConnections }) => {
     const [fontSize, setFontSize] = useState(20);
     const [clickedRemove, setClickRemoved] = useState(false);
     const [truncationLength, setTruncationLength] = useState(25);
+    const [isRemoving, setIsRemoving] = useState(false);
+    const [isAccepting, setIsAccepting] = useState(false);
 
     const truncateString = (str, num) => {
         if (str.length <= num) {
@@ -14,8 +18,9 @@ const AssociatedUser = ({ assistant }) => {
     };
 
     useEffect(() => {
+        console.log("connection", connection);
         const calculateFontSize = () => {
-            const nameLength = `${assistant.firstName} ${assistant.lastName}`.length;
+            const nameLength = `${connection.user_connection.first_name} ${connection.user_connection.last_name}`.length;
             if (nameLength > 20) {
                 setFontSize(Math.max(16, 18 - (nameLength - 20) / 2));
             } else {
@@ -24,7 +29,7 @@ const AssociatedUser = ({ assistant }) => {
         };
 
         calculateFontSize();
-    }, [assistant.firstName, assistant.lastName]);
+    }, [connection.user_connection.first_name, connection.user_connection.last_name]);
 
     const handleRemoveClick = () => {
         setClickRemoved(true);
@@ -36,19 +41,32 @@ const AssociatedUser = ({ assistant }) => {
         setTruncationLength(25); 
     }
 
-    const handleRemoveConfirm = () => {
-        console.log("removed");
+    const handleRemoveConfirm = async () => {
+        setIsRemoving(true);
         // handle an actual removal here
+        try {
+            const response = await deleteUserConnection(connection.user_connection_id);
+            console.log("Removed", response);
+        } catch (error) {
+            console.error('Error:', error);
+        }
+        setIsRemoving(false);
+        getAllUserConnections();
     }
 
-    const handleAccept = () => {
-        console.log("accepted");
+    const handleAccept = async () => {
+        setIsAccepting(true);
         // handle accepting invite here
-    }
-
-    const handleReject = () => {
-        console.log("rejected");
-        // handle rejecting invite here
+        try {
+            connection.user_connection.status = 'confirmed';
+            const userConnectionString = JSON.stringify(connection.user_connection).replace(/"/g, '\\"');
+            const response = await updateUserConnection(connection.user_connection_id, `${userConnectionString}`);
+            console.log("Updated", response);
+        } catch (error) {
+            console.error('Error:', error);
+        }
+        setIsAccepting(false);
+        getAllUserConnections();
     }
 
     return (
@@ -56,26 +74,32 @@ const AssociatedUser = ({ assistant }) => {
             <div className="flex flex-col justify-center">
                 <div className="flex justify-between items-start">
                     <div>
-                        <h3 className="card-title" style={{ fontSize: `${fontSize}px` }} title={`${assistant.firstName} ${assistant.lastName}`}>
-                        {truncateString(`${assistant.firstName} ${assistant.lastName}`, truncationLength)}
+                        <h3 className="card-title" style={{ fontSize: `${fontSize}px` }} title={`${connection.user_connection.first_name} ${connection.user_connection.last_name}`}>
+                        {truncateString(`${connection.user_connection.first_name} ${connection.user_connection.last_name}`, truncationLength)}
                         </h3>
-                        <p className="truncate">{assistant.department}</p>
+                        <p className="truncate">{connection.user_connection.email}</p>
                     </div>
-                    {!clickedRemove && assistant.status === "confirmed" && <button onClick={handleRemoveClick}><TiDelete className="text-error w-6 h-6 m-0" /></button>}
-                    {clickedRemove && assistant.status === "confirmed" && (
+                    {!clickedRemove && connection.user_connection.status === "confirmed" && <button onClick={handleRemoveClick}><TiDelete className="text-error w-6 h-6 m-0" /></button>}
+                    {clickedRemove && connection.user_connection.status === "confirmed" && (
                     <div className="flex flex-col space-y-1">
                         <button onClick={handleCancel} className="text-white btn btn-primary min-h-0 h-6 leading-tight">Cancel</button>
-                        <button onClick={handleRemoveConfirm} className="text-white btn btn-warning min-h-0 h-6 ml-auto leading-tight">Remove</button>
+                        <button onClick={handleRemoveConfirm} className="text-white btn btn-warning min-h-0 h-6 ml-auto leading-tight" disabled={isRemoving}>
+                            {isRemoving ? 'Removing...' : 'Remove'}
+                        </button>
                     </div>
                 )}
                 </div>
                 
             </div>
             <div className="card-actions">
-                {assistant.status === "pending" && (
+                {connection.user_connection.status === "pending" && (
                     <div className="flex justify-between w-full mt-2">
-                        <button onClick={handleReject} className="text-white btn btn-warning min-h-0 h-6 leading-tight">Reject</button>
-                        <button onClick={handleAccept} className="text-white btn btn-success min-h-0 h-6 leading-tight mb-1">Accept</button>
+                        <button onClick={handleRemoveConfirm} className="text-white btn btn-warning min-h-0 h-6 leading-tight" disabled={isRemoving}>
+                            {isRemoving ? 'Declining...' : 'Decline'}
+                        </button>
+                        <button onClick={handleAccept} className="text-white btn btn-success min-h-0 h-6 leading-tight mb-1" disabled={isAccepting}>
+                            {isAccepting ? 'Accepting...' : 'Accept'}
+                        </button>
                     </div>
                 )}
             </div>
--- frontend/src/Views/Assistants.jsx
+++ frontend/src/Views/Assistants.jsx
@@ -2,124 +2,153 @@ import React, { useState, useEffect } from 'react';
 import PageContainer from './PageContainer.jsx';
 import FacultyMenu from '../Components/FacultyMenu';
 import AssociatedUser from '../Components/AssociatedUser.jsx';
+import { getUserConnections } from '../graphql/graphqlHelpers.js';
 
-const assistants = [
-  {
-    id: 1,
-    firstName: 'Alice Betty Joan',
-    lastName: 'Johnson Smith Carter',
-    department: 'Anatomy',
-    faculty: 'Faculty of Medicine',
-    status: 'confirmed'
-  },
-  {
-    id: 2,
-    firstName: 'Bob',
-    lastName: 'Smith',
-    department: 'Physiology',
-    faculty: 'Faculty of Medicine',
-    status: 'confirmed'
-  },
-  {
-    id: 3,
-    firstName: 'Carol',
-    lastName: 'Davis',
-    department: 'Biochemistry',
-    faculty: 'Faculty of Medicine',
-    status: 'confirmed'
-  },
-  {
-    id: 4,
-    firstName: 'David',
-    lastName: 'Martinez',
-    department: 'Pharmacology',
-    faculty: 'Faculty of Medicine',
-    status: 'pending'
-  },
-  {
-    id: 5,
-    firstName: 'Eva',
-    lastName: 'Brown',
-    department: 'Microbiology',
-    faculty: 'Faculty of Medicine',
-    status: 'confirmed'
-  },
-  {
-    id: 6,
-    firstName: 'Frank',
-    lastName: 'Wilson',
-    department: 'Pathology',
-    faculty: 'Faculty of Medicine',
-    status: 'confirmed'
-  }
-];
+// const assistants = [
+//   {
+//     id: 1,
+//     firstName: 'Alice Betty Joan',
+//     lastName: 'Johnson Smith Carter',
+//     department: 'Anatomy',
+//     faculty: 'Faculty of Medicine',
+//     status: 'confirmed'
+//   },
+//   {
+//     id: 2,
+//     firstName: 'Bob',
+//     lastName: 'Smith',
+//     department: 'Physiology',
+//     faculty: 'Faculty of Medicine',
+//     status: 'confirmed'
+//   },
+//   {
+//     id: 3,
+//     firstName: 'Carol',
+//     lastName: 'Davis',
+//     department: 'Biochemistry',
+//     faculty: 'Faculty of Medicine',
+//     status: 'confirmed'
+//   },
+//   {
+//     id: 4,
+//     firstName: 'David',
+//     lastName: 'Martinez',
+//     department: 'Pharmacology',
+//     faculty: 'Faculty of Medicine',
+//     status: 'pending'
+//   },
+//   {
+//     id: 5,
+//     firstName: 'Eva',
+//     lastName: 'Brown',
+//     department: 'Microbiology',
+//     faculty: 'Faculty of Medicine',
+//     status: 'confirmed'
+//   },
+//   {
+//     id: 6,
+//     firstName: 'Frank',
+//     lastName: 'Wilson',
+//     department: 'Pathology',
+//     faculty: 'Faculty of Medicine',
+//     status: 'confirmed'
+//   }
+// ];
 
 const Assistants = ({ userInfo, getCognitoUser }) => {
-  const [user, setUser] = useState(userInfo);
   const [searchTerm, setSearchTerm] = useState('');
+  const [pendingConnections, setPendingConnections] = useState([]);
+  const [confirmedConnections, setConfirmedConnections] = useState([]);
+  const [loading, setLoading] = useState(true);
+  //TODO: FIX SEARCH FUNCTIONALITY
 
   useEffect(() => {
-    setUser(userInfo);
-  }, [userInfo]);
+    getAllUserConnections();
+  }, []);
+
+  const getAllUserConnections = async () => {
+    setLoading(true);
+    try {
+      const retrievedUserConnections = await getUserConnections(userInfo.user_id);
+
+      // Parse the user_connection field from a JSON string to a JSON object
+      const parsedUserConnections = retrievedUserConnections.map(connection => ({
+        ...connection,
+        user_connection: JSON.parse(connection.user_connection),
+      }));
+
+      const filteredUserConnections = parsedUserConnections.filter(connection => 
+        connection.user_connection.first_name.toLowerCase().startsWith(searchTerm.toLowerCase()) ||
+        connection.user_connection.last_name.toLowerCase().startsWith(searchTerm.toLowerCase()) ||
+        connection.user_connection.email.toLowerCase().startsWith(searchTerm.toLowerCase())
+      );
+      
+      console.log('User connections', retrievedUserConnections);
+      setPendingConnections(filteredUserConnections.filter(connection => connection.user_connection.status === 'pending'));
+      setConfirmedConnections(filteredUserConnections.filter(connection => connection.user_connection.status === 'confirmed'));
+    } catch (error) {
+      console.error('Error:', error);
+    }
+    setLoading(false);
+  }
 
   const handleSearchChange = (event) => {
     setSearchTerm(event.target.value);
   };
 
-  const filteredAssistants = assistants.filter(assistant => 
-    assistant.firstName.toLowerCase().startsWith(searchTerm.toLowerCase()) ||
-    assistant.lastName.toLowerCase().startsWith(searchTerm.toLowerCase()) ||
-    assistant.department.toLowerCase().startsWith(searchTerm.toLowerCase())
-  );
-
-  const pendingAssistants = filteredAssistants.filter(assistant => assistant.status === 'pending');
-  const confirmedAssistants = filteredAssistants.filter(assistant => assistant.status === 'confirmed');
-
   return (
     <PageContainer>
-      <FacultyMenu userName={user.preferred_name || user.first_name} getCognitoUser={getCognitoUser} />
+      <FacultyMenu userName={userInfo.preferred_name || userInfo.first_name} getCognitoUser={getCognitoUser} />
       <main className='flex-1 !overflow-auto !h-full custom-scrollbar'>
         <h1 className="text-left ml-4 mt-4 text-4xl font-bold text-zinc-600">Assistants</h1>
-        <div className='m-4 max-w-lg flex'>
-          <label className="input input-bordered flex items-center gap-2 flex-1">
-            <input
-              type="text"
-              className="grow"
-              placeholder="Search"
-              value={searchTerm}
-              onChange={handleSearchChange}
-            />
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 16 16"
-              fill="currentColor"
-              className="h-4 w-4 opacity-70"
-            >
-              <path
-                fillRule="evenodd"
-                d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-                clipRule="evenodd" />
-            </svg>
-          </label>
-        </div>
+        {loading ? (
+            <div className='flex items-center justify-center w-full'>
+              <div className="block text-m mb-1 mt-6 text-zinc-600">Loading...</div>
+            </div>
+          ) : (
+            <div>
+              <div className='m-4 max-w-lg flex'>
+                <label className="input input-bordered flex items-center gap-2 flex-1">
+                  <input
+                    type="text"
+                    className="grow"
+                    placeholder="Search"
+                    value={searchTerm}
+                    onChange={handleSearchChange}
+                  />
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 16 16"
+                    fill="currentColor"
+                    className="h-4 w-4 opacity-70"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+                      clipRule="evenodd" />
+                  </svg>
+                </label>
+              </div>
 
-        {pendingAssistants.length > 0 && 
-          <h2 className="text-left m-4 text-2xl font-bold text-zinc-600">Invitations</h2>
-        }
-        <div className='ml-4 mr-2 flex flex-wrap gap-4'>
-          {pendingAssistants.map((assistant) => (
-            <AssociatedUser key={assistant.id} assistant={assistant} />
-          ))}
-        </div>
-        
-        {confirmedAssistants.length > 0 &&
-          <h2 className="text-left m-4 text-2xl font-bold text-zinc-600">Active</h2>
-        }
-        <div className='ml-4 mr-2 flex flex-wrap gap-4'>
-          {confirmedAssistants.map((assistant) => (
-            <AssociatedUser key={assistant.id} assistant={assistant} />
-          ))}
-        </div>
+              {pendingConnections.length > 0 && 
+                <h2 className="text-left m-4 text-2xl font-bold text-zinc-600">Invitations</h2>
+              }
+              <div className='ml-4 mr-2 flex flex-wrap gap-4'>
+                {pendingConnections.map((connection) => (
+                  <AssociatedUser key={connection.user_connection_id} connection={connection} getAllUserConnections={getAllUserConnections}/>
+                ))}
+              </div>
+              
+              {confirmedConnections.length > 0 &&
+                <h2 className="text-left m-4 text-2xl font-bold text-zinc-600">Active</h2>
+              }
+              <div className='ml-4 mr-2 flex flex-wrap gap-4'>
+                {confirmedConnections.map((connection) => (
+                  <AssociatedUser key={connection.user_connection_id} connection={connection} getAllUserConnections={getAllUserConnections}/>
+                ))}
+              </div>
+            </div>
+          )}
       </main>
     </PageContainer>
   );
