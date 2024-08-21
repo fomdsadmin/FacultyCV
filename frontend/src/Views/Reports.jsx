cf9d765670dcc5ada9177dfc1a903a26d0e29ae1
Kramer <aletheak@ead.ubc.ca>
2024-08-21 11:53:20 -0700

    redesigned the reports page and created placeholders for hooking up to backend

--- frontend/src/Views/Reports.jsx
+++ frontend/src/Views/Reports.jsx
@@ -1,44 +1,72 @@
 import React, { useState, useEffect } from 'react';
 import PageContainer from './PageContainer.jsx';
 import FacultyMenu from '../Components/FacultyMenu';
-import { getAllSections, getUserCVData } from '../graphql/graphqlHelpers.js';
+import { getAllSections, getAllTemplates, getUserCVData } from '../graphql/graphqlHelpers.js';
 import '../CustomStyles/scrollbar.css';
 import Report from '../Components/Report.jsx';
-
-const mockPrevReports = ["Grants 2024", "Teaching 2023", "Publications 2022"];
 
 const Reports = ({ userInfo, getCognitoUser }) => {
   const [user, setUser] = useState(userInfo);
   const [selectedTemplate, setSelectedTemplate] = useState('');
-  const [loading, setLoading] = useState(true); // Add loading state
+  const [loading, setLoading] = useState(true); 
   const [dataSections, setDataSections] = useState([]);
+  const [Templates, setTemplates] = useState([]);
+  const [latex, setLatex] = useState('');
+  const [buildingLatex, setBuildingLatex] = useState(false);
 
   useEffect(() => {
-    setUser(userInfo);
-    //getDataSections();
-  }, [userInfo]);
+    const fetchData = async () => {
+      setUser(userInfo);
+      
+      const templates = await getAllTemplates();
+      setTemplates(templates);
+  
+    };
+  
+    fetchData();
+  }, [userInfo]); 
 
-  useEffect(() => {
-    if (dataSections.length > 0) {
-      buildLatex();
-    }
-  }, [dataSections]);
 
-  const getDataSections = async () => {
+  const handleTemplateChange = (template) => {
+    setSelectedTemplate(template);
+    // console.log("THIS IS TEMPLATE:" + template.data_section_ids);
+    getDataSections(template);
+  };
+  
+
+  const getDataSections = async (template) => {
     const retrievedSections = await getAllSections();
 
-    // Parse the attributes field from a JSON string to a JSON object
+    const sectionIds = template.data_section_ids;
+
     const parsedSections = retrievedSections.map(section => ({
       ...section,
       attributes: JSON.parse(section.attributes),
     }));
 
-    console.log(parsedSections);
-    setDataSections(parsedSections);
-    console.log("Data Sections:" + dataSections);
-    console.log("building Latex");
+    let filteredSections = [];
+
+    if (sectionIds != null && sectionIds.length > 0) { 
+      // console.log("Section IDs: " + sectionIds);
+      filteredSections = parsedSections.filter(section =>
+        sectionIds.includes(section.data_section_id)
+      );
+    } else {
+      // console.log("no template selected");
+      // console.log("sectionIds: " + sectionIds);
+      filteredSections = parsedSections;
+    }
+
+    // console.log(filteredSections);
+    setDataSections(filteredSections);
+    // console.log("Data Sections:" + dataSections);
+    // console.log("building Latex");
+
+    setBuildingLatex(true);
+    let latex = await buildLatex(filteredSections);
+    setLatex(latex);
+    setBuildingLatex(false); 
     setLoading(false);
-    console.log(userInfo);
   }
 
   const escapeLatex = (text) => {
@@ -56,7 +84,7 @@ const Reports = ({ userInfo, getCognitoUser }) => {
       .replace(/~/g, '\\textasciitilde');
   };
   
-  const buildLatex = async () => {
+  const buildLatex = async (sections) => {
     let latex = `
 \\documentclass{article}
 \\usepackage[margin=0.5in]{geometry}
@@ -142,10 +170,10 @@ ${escapeLatex(user.campus)} \\\\
 \\end{flushleft}
 `;
   
-    for (const section of dataSections) {
+    for (const section of sections) {
       try {
       
-        console.log(`Fetching data for section: ${section.title}`);
+        // console.log(`Fetching data for section: ${section.title}`);
         let sectionData;
         try {
           sectionData = await getUserCVData(userInfo.user_id, section.data_section_id);
@@ -153,11 +181,10 @@ ${escapeLatex(user.campus)} \\\\
          
         }
         
-        
   
         if (!sectionData || sectionData.length === 0) {
-          console.log(`No data found for section ID: ${section.data_section_id}`);
-          continue; // Skip to the next section if there's no data
+          // console.log(`No data found for section ID: ${section.data_section_id}`);
+          continue;
         }
   
         const parsedData = sectionData.map(data => ({
@@ -165,14 +192,13 @@ ${escapeLatex(user.campus)} \\\\
           data_details: JSON.parse(data.data_details),
         }));
   
-        console.log(`Parsed data for section ${section.title}:`, parsedData);
+        // console.log(`Parsed data for section ${section.title}:`, parsedData);
 
-        // Ensure attributes are properly parsed
         const attributes = JSON.parse(section.attributes);
 
         // Get the attribute keys for the table headers
         const headers = Object.keys(attributes);
-        console.log(`Headers for section ${section.title}:`, headers);
+        // console.log(`Headers for section ${section.title}:`, headers);
   
         latex += `\\subsection*{${escapeLatex(section.title)}}\n`;
         latex += `\\begin{tabularx}{\\textwidth}{| ${headers.map(() => 'X').join(' | ')} |}\n`;
@@ -182,11 +208,10 @@ ${escapeLatex(user.campus)} \\\\
   
         for (const item of parsedData) {
           const row = headers.map(header => {
-            // Convert header to snake_case to match the keys in data_details
             const key = header.replace(/\s+/g, '_').toLowerCase();
             const value = item.data_details[key];
-            console.log(`Value for key ${key} in section ${section.title}:`, value);
-            return escapeLatex(value !== undefined ? value : ''); // Handle missing data
+            // console.log(`Value for key ${key} in section ${section.title}:`, value);
+            return escapeLatex(value !== undefined ? value : ''); 
           }).join(' & ');
           latex += `${row} \\\\ \n`;
           latex += `\\hline\n`;
@@ -195,29 +220,40 @@ ${escapeLatex(user.campus)} \\\\
         latex += `\\end{tabularx}\n\n`;
   
       } catch (error) {
-        console.error(`Error fetching data for section ID: ${section.data_section_id}`, error);
+        // console.error(`Error fetching data for section ID: ${section.data_section_id}`, error);
       }
     }
   
     latex += `\\end{document}`;
-  
-    console.log(latex);
+
   
     return latex;
   }
-  
 
-  const handleSave = () => {
-    // Save report logic
+  const handlePreview = () => {
+    console.log(latex);
+    // this is where we need to grab the corresponding pdf and display it
+    // Need to add PDF viewing functionality
   };
 
-  const getFormattedDate = () => {
-    const date = new Date();
-    const year = date.getFullYear();
-    const month = (date.getMonth() + 1).toString().padStart(2, '0');
-    const day = date.getDate().toString().padStart(2, '0');
-    return `${year}-${month}-${day}`;
-  };
+  const handleDownload = () => {
+    
+    // generating the file name
+    const lastName = user.last_name || 'unknown';
+    const templateTitle = selectedTemplate?.title || 'template';
+    const currentDate = new Date().toISOString().split('T')[0]; // Format: YYYY-MM-DD
+
+    const fileName = `${lastName}/${templateTitle}/${currentDate}.tex`;
+
+    // below is just a test to download the latex file, we need to to be the corresponding pdf
+    const element = document.createElement('a');
+    const file = new Blob([latex], { type: 'text/plain' });
+    element.href = URL.createObjectURL(file);
+    element.download = fileName;
+    document.body.appendChild(element);
+    element.click();
+  }
+
 
   return (
     <PageContainer className="custom-scrollbar">
@@ -226,35 +262,51 @@ ${escapeLatex(user.campus)} \\\\
         <div className='flex w-full h-full'>
           <div className='flex-1 min-w-96 !overflow-auto !h-full custom-scrollbar'>
             <h1 className="text-4xl ml-2 font-bold my-3 text-zinc-600">Reports</h1>
-            <label className="form-control ml-2 w-full max-w-xs">
-              <div className="label">
-                <span className="label-text">Template</span>
+            <h2 className="text-2xl ml-2 font-bold my-3 text-zinc-600">Select a Template</h2>
+
+              <div className='overflow-auto h-auto mb-6 custom-scrollbar'>
+              {Templates.map((template, index) => (
+                <Report key={index} title={template.title} onClick={() => handleTemplateChange(template)}
+                  isSelected={selectedTemplate && selectedTemplate.template_id === template.template_id} 
+                />
+              ))}
               </div>
-              <select 
-                className="select select-bordered"
-                value={selectedTemplate}
-                onChange={(e) => setSelectedTemplate(e.target.value)}
-              >
-                <option disabled value="">Select Template</option>
-                <option value="Teaching">Teaching</option>
-                <option value="Grant_Application">Grant Application</option>
-                <option value="Publications">Publications</option>
-              </select>
-            </label>
-            <label className="ml-2 mt-2 form-control w-full max-w-xs">
-              <div className="label">
-                <span className="label-text">Title</span>
+
+
+            {selectedTemplate && (
+              <div className='sticky bottom-0 ml-2'>
+                <button 
+                  onClick={handlePreview} 
+                  className={`mr-2 mt-6 text-white btn ${buildingLatex ? 'btn-disabled' : 'btn-accent'} min-h-0 h-6 leading-tight mb-1`} 
+                  disabled={buildingLatex}
+                >
+                  {buildingLatex ? (
+                    <>
+                      <span className="loader"></span>
+                    </>
+                  ) : (
+                    'Preview'
+                  )}
+                </button>
+                <button
+                  onClick={handleDownload}
+                  className={`mt-6 text-white btn ${buildingLatex ? 'btn-disabled' : 'btn-success'} min-h-0 h-6 leading-tight mb-1`}
+                  disabled={buildingLatex}
+                >
+                  {buildingLatex ? (
+                    <>
+                      <span className="loader"></span>
+                    </>
+                  ) : (
+                    'Download'
+                  )}
+                </button>
               </div>
-              <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
-            </label>
-            <button onClick={handleSave} className="ml-2 mt-6 text-white btn btn-success min-h-0 h-6 leading-tight mb-1">Save</button>
-            
-            <h2 className="ml-2 mt-10 text-2xl font-bold my-3 text-zinc-600">Previous</h2>
-            {mockPrevReports.map((report, index) => (
-              <Report key={index} title={report} />
-            ))}
+            )}
+
+
+
           </div>
-          <div className='flex-none w-0.5 bg-neutral h-screen' />
  
         </div>
       </main>
