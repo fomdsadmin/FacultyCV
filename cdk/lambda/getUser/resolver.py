9dcda9a092c3d42fb40845263e7866e59d0ec84f
abhiverma13 <a.verma1304@gmail.com>
2024-09-11 10:54:01 -0700

    fixed security issue

--- cdk/lambda/getUser/resolver.py
+++ cdk/lambda/getUser/resolver.py
@@ -1,6 +1,7 @@
 import boto3
 import json
 import psycopg2
+import os
 
 sm_client = boto3.client('secretsmanager')
 
@@ -15,14 +16,32 @@ def getCredentials():
     credentials['db'] = secrets['dbname']
     return credentials
 
-def getUser(arguments):
+def getUser(event):
     credentials = getCredentials()
     connection = psycopg2.connect(user=credentials['username'], password=credentials['password'], host=credentials['host'], database=credentials['db'])
     print("Connected to database")
     cursor = connection.cursor()
     
+    arguments = event['arguments']
+
+    cognito_idp = boto3.client('cognito-idp')
+
+    user_name = event['identity']['claims']['username']
+    user_pool_id = os.environ['USER_POOL_ID']
+
+    # Get user attributes
+    user_attributes = cognito_idp.admin_get_user(
+        UserPoolId=user_pool_id,
+        Username=user_name
+    )
+
+    cognito_email = next(attr['Value'] for attr in user_attributes['UserAttributes'] if attr['Name'] == 'email')
     # Prepare the SELECT query
     email = arguments['email']  # get the email from arguments
+
+    if email != cognito_email:
+        return "Unauthorized"
+
     query = f"""
     SELECT * FROM users 
     WHERE email = '{email}'
@@ -66,4 +85,4 @@ def getUser(arguments):
     return user
 
 def lambda_handler(event, context):
-    return getUser(event['arguments'])
+    return getUser(event)
