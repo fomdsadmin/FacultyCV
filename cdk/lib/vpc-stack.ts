0e75ff73aca7e849ecc8359c6747a0749646c1ec
abhiverma13 <a.verma1304@gmail.com>
2024-09-25 10:07:02 -0700

    vpc changes

--- cdk/lib/vpc-stack.ts
+++ cdk/lib/vpc-stack.ts
@@ -1,4 +1,4 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { Stack, StackProps, CfnParameter } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as ec2 from 'aws-cdk-lib/aws-ec2'
 import * as secretsmanager from "aws-cdk-lib/aws-secretsmanager";
@@ -12,63 +12,118 @@ export class VpcStack extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
       super(scope, id, props);
 
-      const natGatewayProvider = ec2.NatProvider.gateway()
-
-      // VPC for application
-      this.vpc = new ec2.Vpc(this, 'facultyCV-Vpc', {
-          //cidr: '10.0.0.0/16',
-          ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/16'),
-          natGatewayProvider: natGatewayProvider,
-          natGateways: 1,
-          maxAzs: 2,
-          subnetConfiguration: [
-            {
-              name: 'public-subnet-1',
-              subnetType: ec2.SubnetType.PUBLIC,
-            },
-            {
-              name: 'isolated-subnet-1',
-              subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
-            }
-          ],
-          gatewayEndpoints: {
-            S3: {
-              service: ec2.GatewayVpcEndpointAwsService.S3,
-            },
-          },
-      });
-      this.vpc.addFlowLog('facultyCV-vpcFlowLog');
-
-      // Add SSM endpoint to VPC
-      this.vpc.addInterfaceEndpoint("SSM Endpoint", {
-        service: ec2.InterfaceVpcEndpointAwsService.SSM,
-        subnets: {subnetType: ec2.SubnetType.PRIVATE_ISOLATED},
+      // Parameters for existing resources
+      const existingVpcId = new CfnParameter(this, 'existingVpcId', {
+          type: 'String',
+          description: 'The ID of the existing VPC',
+          default: '',
       });
 
-      // Add secrets manager endpoint to VPC
-      this.vpc.addInterfaceEndpoint("Secrets Manager Endpoint", {
-        service: ec2.InterfaceVpcEndpointAwsService.SECRETS_MANAGER,
-        subnets: {subnetType: ec2.SubnetType.PRIVATE_ISOLATED},
+      const existingNatGatewayId = new CfnParameter(this, 'existingNatGatewayId', {
+          type: 'String',
+          description: 'The ID of the existing NAT Gateway',
+          default: '',
       });
 
-      // Add RDS endpoint to VPC
-      this.vpc.addInterfaceEndpoint("RDS Endpoint", {
-        service: ec2.InterfaceVpcEndpointAwsService.RDS,
-        subnets: {subnetType: ec2.SubnetType.PRIVATE_ISOLATED},
+      const existingPublicSubnetId = new CfnParameter(this, 'existingPublicSubnetId', {
+          type: 'String',
+          description: 'The ID of the existing Public Subnet',
+          default: '',
       });
 
-      // Add Glue endpoint to VPC
-      this.vpc.addInterfaceEndpoint("Glue Endpoint", {
-        service: ec2.InterfaceVpcEndpointAwsService.GLUE,
-        subnets: {subnetType: ec2.SubnetType.PRIVATE_ISOLATED}
+      const existingIsolatedSubnetId = new CfnParameter(this, 'existingIsolatedSubnetId', {
+          type: 'String',
+          description: 'The ID of the existing Isolated Subnet',
+          default: '',
       });
 
-      this.vpc.isolatedSubnets.forEach(({ routeTable: { routeTableId } }, index) => {
-        new ec2.CfnRoute(this, 'PrivateSubnetPeeringConnectionRoute' + index, {
-          destinationCidrBlock: '0.0.0.0/0',
-          routeTableId,
-          natGatewayId: natGatewayProvider.configuredGateways[0].gatewayId
-        })
-      })
+      // Check if existing VPC ID is provided
+      if (existingVpcId.valueAsString !== '' && existingNatGatewayId.valueAsString !== '' && existingPublicSubnetId.valueAsString !== '' && existingIsolatedSubnetId.valueAsString !== '') {
+          // Use existing VPC
+          // You can either use vpcId OR vpcName and fetch the desired vpc
+          const getExistingVpc = ec2.Vpc.fromLookup(this, 'ImportVPC',{
+            vpcId: existingVpcId.valueAsString
+            //vpcName: "VPC_NAME"
+          }) as ec2.Vpc;
+          this.vpc = getExistingVpc;
+          // this.vpc = ec2.Vpc.fromVpcAttributes(this, 'ExistingVpc', {
+          //     vpcId: existingVpcId.valueAsString,
+          //     availabilityZones: ['us-west-2a', 'us-west-2b'], // Adjust as needed
+          //     publicSubnetIds: [existingPublicSubnetId.valueAsString],
+          //     isolatedSubnetIds: [existingIsolatedSubnetId.valueAsString],
+          // });
+          // Use existing subnets and NAT Gateway
+          const publicSubnet = ec2.Subnet.fromSubnetId(this, 'PublicSubnet', existingPublicSubnetId.valueAsString);
+          const isolatedSubnet = ec2.Subnet.fromSubnetId(this, 'IsolatedSubnet', existingIsolatedSubnetId.valueAsString);
+
+          // Add routes to the existing subnets
+          new ec2.CfnRoute(this, 'PublicSubnetRoute', {
+              destinationCidrBlock: '0.0.0.0/0',
+              routeTableId: publicSubnet.routeTable.routeTableId,
+              natGatewayId: existingNatGatewayId.valueAsString,
+          });
+
+          new ec2.CfnRoute(this, 'IsolatedSubnetRoute', {
+              destinationCidrBlock: '0.0.0.0/0',
+              routeTableId: isolatedSubnet.routeTable.routeTableId,
+              natGatewayId: existingNatGatewayId.valueAsString,
+          });
+      } else {
+          // Create new VPC
+          const natGatewayProvider = ec2.NatProvider.gateway();
+
+          this.vpc = new ec2.Vpc(this, 'facultyCV-Vpc', {
+              ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/16'),
+              natGatewayProvider: natGatewayProvider,
+              natGateways: 1,
+              maxAzs: 2,
+              subnetConfiguration: [
+                  {
+                      name: 'public-subnet-1',
+                      subnetType: ec2.SubnetType.PUBLIC,
+                  },
+                  {
+                      name: 'isolated-subnet-1',
+                      subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+                  },
+              ],
+              gatewayEndpoints: {
+                  S3: {
+                      service: ec2.GatewayVpcEndpointAwsService.S3,
+                  },
+              },
+          });
+
+          this.vpc.addFlowLog('facultyCV-vpcFlowLog');
+
+          // Add endpoints to VPC
+          this.vpc.addInterfaceEndpoint('SSM Endpoint', {
+              service: ec2.InterfaceVpcEndpointAwsService.SSM,
+              subnets: { subnetType: ec2.SubnetType.PRIVATE_ISOLATED },
+          });
+
+          this.vpc.addInterfaceEndpoint('Secrets Manager Endpoint', {
+              service: ec2.InterfaceVpcEndpointAwsService.SECRETS_MANAGER,
+              subnets: { subnetType: ec2.SubnetType.PRIVATE_ISOLATED },
+          });
+
+          this.vpc.addInterfaceEndpoint('RDS Endpoint', {
+              service: ec2.InterfaceVpcEndpointAwsService.RDS,
+              subnets: { subnetType: ec2.SubnetType.PRIVATE_ISOLATED },
+          });
+
+          this.vpc.addInterfaceEndpoint('Glue Endpoint', {
+              service: ec2.InterfaceVpcEndpointAwsService.GLUE,
+              subnets: { subnetType: ec2.SubnetType.PRIVATE_ISOLATED },
+          });
+
+          this.vpc.isolatedSubnets.forEach(({ routeTable: { routeTableId } }, index) => {
+              new ec2.CfnRoute(this, 'PrivateSubnetPeeringConnectionRoute' + index, {
+                  destinationCidrBlock: '0.0.0.0/0',
+                  routeTableId,
+                  natGatewayId: natGatewayProvider.configuredGateways[0].gatewayId,
+              });
+          });
+      }
   }
 }
