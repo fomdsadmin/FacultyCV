42acabeb4338112482d9bc6b27352e9d3d0baed7
Behl <aayush.behl@ubc.ca>
2024-11-05 19:10:02 -0800

    Metric changed to number of users with CVs

--- cdk/lambda/getNumberOfGeneratedCVs/resolver.py
+++ cdk/lambda/getNumberOfGeneratedCVs/resolver.py
@@ -4,11 +4,11 @@ import os
 def getNumberOfGeneratedCVs(arguments):
     client = boto3.client('s3')
     response = client.list_objects_v2(Bucket=os.environ['BUCKET_NAME'])
-    numCVs = 0
+    usersWithCvs = set()
     for item in response['Contents']:
         if item['Key'].endswith('.pdf'):
-            numCVs += 1
-    return numCVs
+            usersWithCvs.add(item['Key'][0:item['Key'].index('/')])
+    return len(usersWithCvs)
 
 def lambda_handler(event, context):
     return getNumberOfGeneratedCVs(event['arguments'])
