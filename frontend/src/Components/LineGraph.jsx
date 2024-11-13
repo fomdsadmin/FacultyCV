4d403a726b658005efbe4d069dce9fea09806882
pepkh <nkhushi184@gmail.com>
2024-11-13 10:34:08 -0800

    Grant Secured Over Time graph added

--- frontend/src/Components/LineGraph.jsx
+++ frontend/src/Components/LineGraph.jsx
@@ -1,16 +1,21 @@
-import { CartesianGrid, LineChart, ResponsiveContainer, YAxis, XAxis, Tooltip, Legend, Line} from 'recharts';
+import { CartesianGrid, LineChart, ResponsiveContainer, YAxis, XAxis, Tooltip, Legend, Line } from 'recharts';
 
-export const LineGraph = ({ data }) => {
+export const LineGraph = ({ data, dataKey, lineColor }) => {
     return (
         <ResponsiveContainer width="100%" height="100%">
-            <LineChart data={data}>
+            <LineChart data={data}
+            margin={{ left: 50 }}>
+                
                 <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="date" />
-                <YAxis />
+                <XAxis 
+                    dataKey="date" 
+                    tickFormatter={(tick) => tick} // Directly use the string provided in the `date` field
+                />
+                <YAxis allowDecimals={false} />
                 <Tooltip />
                 <Legend />
-                <Line type="monotone" dataKey="Users" stroke="#8884d8" activeDot={{ r: 8 }} />
+                <Line type="monotone" dataKey={dataKey} stroke={lineColor || "#8884d8"} activeDot={{ r: 8 }} />
             </LineChart>  
         </ResponsiveContainer>
-    )
-}
+    );
+};
