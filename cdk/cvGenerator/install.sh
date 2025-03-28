c009195b4e370c1e3d84a13ddfc54062ff147fcc
fomdsadmin <fomds.sys@ubc.ca>
2025-03-28 10:29:57 -0700

    Cleaning up comented out code from old tests

--- cdk/cvGenerator/install.sh
+++ cdk/cvGenerator/install.sh
@@ -6,10 +6,6 @@ zcat < install-tl-unx.tar.gz | tar xf - --strip-components=1 -C latex_install
 
 rm install-tl-unx.tar.gz
 
-#echo 'selected_scheme scheme-small' > temp.profile
-
-#output=$(perl ./latex_install/install-tl --no-interaction --paper=letter --profile temp.profile --location "https://ca.mirrors.cicku.me/ctan/systems/texlive/tlnet")
-
 output=$(perl ./latex_install/install-tl --no-interaction --paper=letter --scheme=scheme-basic --location "https://ca.mirrors.cicku.me/ctan/systems/texlive/tlnet")
 
 path_line=$(echo "$output" | grep "Most importantly,")
