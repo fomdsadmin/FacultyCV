c576cf51efda83911461fc24abe69b00b853d130
fomdsadmin <fomds.sys@ubc.ca>
2025-03-28 10:12:59 -0700

    Changes to Docker files and permissions to allow the build to work on ubuntu

--- cdk/cvGenerator/install.sh
+++ cdk/cvGenerator/install.sh
@@ -6,7 +6,11 @@ zcat < install-tl-unx.tar.gz | tar xf - --strip-components=1 -C latex_install
 
 rm install-tl-unx.tar.gz
 
-output=$(perl ./latex_install/install-tl --no-interaction --paper=letter --scheme=basic)
+#echo 'selected_scheme scheme-small' > temp.profile
+
+#output=$(perl ./latex_install/install-tl --no-interaction --paper=letter --profile temp.profile --location "https://ca.mirrors.cicku.me/ctan/systems/texlive/tlnet")
+
+output=$(perl ./latex_install/install-tl --no-interaction --paper=letter --scheme=scheme-basic --location "https://ca.mirrors.cicku.me/ctan/systems/texlive/tlnet")
 
 path_line=$(echo "$output" | grep "Most importantly,")
 
