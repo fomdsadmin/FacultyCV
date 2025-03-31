2617bd867d787c95c5f2d6898e06bc5a9f37f022
Naveen Karduri <naveen.karduri@ubc.ca>
2025-03-31 11:33:23 -0700

    Updated Rise/Grants Data pull to check with First Name and Last Name combination only

--- cdk/lambda/getSecureFundingMatches/resolver.py
+++ cdk/lambda/getSecureFundingMatches/resolver.py
@@ -13,12 +13,13 @@ def getSecureFundingMatches(arguments):
     cursor = connection.cursor()
 
     # Retrieve results with the same last name
-    cursor.execute('SELECT * FROM grants WHERE last_name = %s', (arguments['last_name'],))
+    #cursor.execute('SELECT * FROM grants WHERE last_name = %s', (arguments['last_name'],))
+    cursor.execute('SELECT * FROM grants WHERE first_name = %s AND last_name = %s', (arguments['first_name'], arguments['last_name']))
     results_same_last = cursor.fetchall()
 
     # Retrieve results with the same first name
-    cursor.execute('SELECT * FROM grants WHERE first_name = %s', (arguments['first_name'],))
-    results_same_first = cursor.fetchall()
+    # cursor.execute('SELECT * FROM grants WHERE first_name = %s', (arguments['first_name'],))
+    # results_same_first = cursor.fetchall()
 
     cursor.close()
     connection.close()
@@ -45,24 +46,24 @@ def getSecureFundingMatches(arguments):
                 }
             })
 
-    if len(results_same_first) > 0:
-        for result in results_same_first:
-            matches.append({
-                'secure_funding_id': result[0],
-                'first_name': result[1],
-                'last_name': result[2],
-                'data_details': {
-                    'first_name': result[1],
-                    'last_name': result[2],
-                    'keywords': result[3],
-                    'agency': result[4],
-                    'department': result[5],
-                    'program': result[6],
-                    'title': result[7],
-                    'amount': result[8],
-                    'dates': result[9]
-                }
-            })
+    # if len(results_same_first) > 0:
+        # for result in results_same_first:
+            # matches.append({
+                # 'secure_funding_id': result[0],
+                # 'first_name': result[1],
+                # 'last_name': result[2],
+                # 'data_details': {
+                    # 'first_name': result[1],
+                    # 'last_name': result[2],
+                    # 'keywords': result[3],
+                    # 'agency': result[4],
+                    # 'department': result[5],
+                    # 'program': result[6],
+                    # 'title': result[7],
+                    # 'amount': result[8],
+                    # 'dates': result[9]
+                # }
+            # })
 
     return matches
 
