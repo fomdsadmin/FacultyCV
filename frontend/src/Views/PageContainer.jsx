c7a31e4bf4d7e97fac79ac54f587361e7791feeb
Naveen Karduri <naveen.karduri@ubc.ca>
2025-06-04 09:09:35 -0700

    Minor issue: Double Scrolling on the pages

--- frontend/src/Views/PageContainer.jsx
+++ frontend/src/Views/PageContainer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const PageContainer = ({ children }) => {
   return (
-    <div className="mx-auto flex min-h-screen max-h-screen h-screen box-border">
+    <div className="mx-auto flex min-h-screen box-border">
       {children}
     </div>
   );
