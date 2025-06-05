7ccff020077f3201ca0c7cc0304ae6c222efb579
Jake Yeo <JakeYeoZh@gmail.com>
2025-06-05 12:19:58 -0700

    Add support for default modal styling in ModalStylingWrapper component

--- frontend/src/SharedComponents/ModalStylingWrapper.jsx
+++ frontend/src/SharedComponents/ModalStylingWrapper.jsx
@@ -1,10 +1,16 @@
 import { createPortal } from "react-dom"
 
 // Wrap all your dialog components with this
-const ModalStylingWrapper = ({ children }) => {
+const ModalStylingWrapper = ({ children, useDefaultBox = false }) => {
     return createPortal(
         <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-            {children}
+            {useDefaultBox ? <div className="bg-white rounded-lg shadow-lg p-6 max-w-md w-full mx-4 relative">
+                {children}
+            </div> :
+                <>
+                    {children}
+                </>
+            }
         </div>,
         document.body
     )
