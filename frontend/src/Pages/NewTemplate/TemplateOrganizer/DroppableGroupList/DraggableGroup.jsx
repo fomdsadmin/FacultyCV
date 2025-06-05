6c4b07b34c0f1041edbb511928c65ef6f0dc46b5
Jake Yeo <JakeYeoZh@gmail.com>
2025-06-05 12:19:43 -0700

    Refactor DraggableGroup to include group removal functionality and improve layout

--- frontend/src/Pages/NewTemplate/TemplateOrganizer/DroppableGroupList/DraggableGroup.jsx
+++ frontend/src/Pages/NewTemplate/TemplateOrganizer/DroppableGroupList/DraggableGroup.jsx
@@ -1,7 +1,30 @@
 import { Draggable } from "react-beautiful-dnd"
 import DroppableSectionList from "../DroppableSectionList/DroppableSectionList"
+import { useTemplate } from "../../TemplateContext";
+import { FaTimesCircle } from "react-icons/fa";
 
 const DraggableGroup = ({ group, groupIndex }) => {
+
+    const { DEFAULT_GROUP_ID, groups, setGroups } = useTemplate();
+
+    const onRemoveGroup = () => {
+
+        var updatedGroups = [...groups];
+
+        const indexOfDefaultGroupToModify = groups.findIndex((group) => group.id === DEFAULT_GROUP_ID);
+        const indexOfCurrentGroupToDelete = groups.findIndex((grp) => grp.id === group.id);
+        const defaultGroup = groups[indexOfDefaultGroupToModify];
+        const updatedDefaultGroupSections = [...groups[indexOfCurrentGroupToDelete].sections, ...defaultGroup.sections];
+        updatedGroups[indexOfDefaultGroupToModify] = {
+            ...defaultGroup,
+            sections: updatedDefaultGroupSections
+        };
+
+        updatedGroups = updatedGroups.filter((grp) => grp.id !== group.id);
+
+        setGroups(updatedGroups);
+    }
+
     return <Draggable draggableId={group.id} index={groupIndex}>
         {(provided) => (
             <div
@@ -9,9 +32,16 @@ const DraggableGroup = ({ group, groupIndex }) => {
                 ref={provided.innerRef}
                 {...provided.draggableProps}
             >
-                <h2 className="font-bold mb-2" {...provided.dragHandleProps}>
-                    {group.title}
-                </h2>
+                <div className="flex items-center justify-between justify-center w-full pl-3 pr-4 pt-3">
+                    <h2 className="font-bold mb-2" {...provided.dragHandleProps}>
+                        {group.title}
+                    </h2>
+                    {group.id !== DEFAULT_GROUP_ID && (
+                        <button onClick={onRemoveGroup} className="btn btn-xs btn-circle btn-ghost">
+                            <FaTimesCircle className="h-6 w-6 text-red-500" />
+                        </button>
+                    )}
+                </div>
                 <DroppableSectionList group={group}></DroppableSectionList>
             </div>
         )}
