17ef1fb4db8f6143e0041fd221f3d93f5c163399
Jake Yeo <JakeYeoZh@gmail.com>
2025-06-06 14:40:55 -0700

    Refactored removeSectionButton into it own components

--- frontend/src/Pages/NewTemplate/TemplateOrganizer/DroppableGroupList/DroppableSectionList/DraggableSection.jsx
+++ frontend/src/Pages/NewTemplate/TemplateOrganizer/DroppableGroupList/DroppableSectionList/DraggableSection.jsx
@@ -1,37 +1,17 @@
 import { useTemplate } from "Pages/NewTemplate/TemplateContext"
 import { Draggable } from "react-beautiful-dnd"
-import { FaTimesCircle, FaGripVertical } from "react-icons/fa"
+import { FaGripVertical } from "react-icons/fa"
 import DroppableAttributeGroupList from "./DroppableAttributeGroupList/DroppableAttributeGroupList"
 import { Accordion } from "SharedComponents/Accordion/Accordion"
 import { AccordionItem } from "SharedComponents/Accordion/AccordionItem"
 import AddAttributeGroupButton from "./AddAttributeGroupButton/AddAttributeGroupButton"
+import RemoveSectionButton from "./RemoveSectionButton/RemoveSectionButton"
 
 const DraggableSection = ({ draggableId, preparedSectionIndex, preparedSection, isInHiddenGroup }) => {
 
-    const { getGroupIdContainingPreparedSectionId, HIDDEN_GROUP_ID, groups, setGroups } = useTemplate();
+    const { getGroupIdContainingPreparedSectionId } = useTemplate();
 
     const currentGroupId = getGroupIdContainingPreparedSectionId(preparedSection.data_section_id);
-
-    const handleRemoveSection = () => {
-        const indexOfGroupToModify = groups.findIndex((group) => group.id === currentGroupId);
-        const group = groups[indexOfGroupToModify];
-        const updatedPreparedSections = group.prepared_sections.filter(s => s.data_section_id !== preparedSection.data_section_id);
-        const updatedGroups = [...groups];
-        updatedGroups[indexOfGroupToModify] = {
-            ...group,
-            prepared_sections: updatedPreparedSections
-        };
-
-        const indexOfDefaultGroupToModify = groups.findIndex((group) => group.id === HIDDEN_GROUP_ID);
-        const defaultGroup = groups[indexOfDefaultGroupToModify];
-        const updatedDefaultGroupPreparedSections = [preparedSection, ...defaultGroup.prepared_sections];
-        updatedGroups[indexOfDefaultGroupToModify] = {
-            ...defaultGroup,
-            prepared_sections: updatedDefaultGroupPreparedSections
-        };
-
-        setGroups(updatedGroups);
-    }
 
     return <Draggable
         draggableId={draggableId}
@@ -53,18 +33,8 @@ const DraggableSection = ({ draggableId, preparedSectionIndex, preparedSection, 
                         </div>
                     </div>
                     <div className="flex items-center gap-2 pr-4">
-                        <AddAttributeGroupButton groupId={currentGroupId} dataSectionId={preparedSection.data_section_id}/>
-                        {getGroupIdContainingPreparedSectionId(preparedSection.data_section_id) !== HIDDEN_GROUP_ID && (
-                            <button
-                                onClick={(e) => {
-                                    e.stopPropagation();
-                                    handleRemoveSection();
-                                }}
-                                className="btn btn-xs btn-circle btn-ghost"
-                            >
-                                <FaTimesCircle className="h-6 w-6 text-red-500" />
-                            </button>
-                        )}
+                        <AddAttributeGroupButton groupId={currentGroupId} dataSectionId={preparedSection.data_section_id} />
+                        <RemoveSectionButton preparedSection={preparedSection} />
                     </div>
                 </div>
             );
