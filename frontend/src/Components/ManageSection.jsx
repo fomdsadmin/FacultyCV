d7947cc2b459d730432498a968443df705bede28
Sawhney <a.sawhney@ubc.ca>
2025-06-10 12:02:08 -0700

    Added update btn for admin section manage

--- frontend/src/Components/ManageSection.jsx
+++ frontend/src/Components/ManageSection.jsx
@@ -3,10 +3,13 @@ import { useState } from 'react';
 import { FaArrowLeft, FaTrash } from 'react-icons/fa';
 import DeleteSectionModal from './DeleteSectionModal';
 import AddAttributeModal from './AddAttributeModal.jsx';
+import AttributeModal from "./AttributeModal.jsx";
 
 const ManageSection = ({ section, onBack, getDataSections }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isAddAttributeModalOpen, setIsAddAttributeModalOpen] = useState(false);
+  const [isUpdateAttributeModalOpen, setIsUpdateAttributeModalOpen] =
+    useState(false);
 
   const handleBack = () => {
     onBack();
@@ -20,38 +23,90 @@ const ManageSection = ({ section, onBack, getDataSections }) => {
     setIsAddAttributeModalOpen(true);
   };
 
-  const attributes = typeof section.attributes === 'string' ? JSON.parse(section.attributes) : section.attributes;
+  const handleUpdateAttributeClick = () => {
+    setIsUpdateAttributeModalOpen(true);
+  };
+
+  const attributes =
+    typeof section.attributes === "string"
+      ? JSON.parse(section.attributes)
+      : section.attributes;
 
   return (
     <div>
       <div className="flex justify-between items-center pt-4">
-        <button onClick={handleBack} className='text-zinc-800 btn btn-ghost min-h-0 h-8 leading-tight mr-4'>
+        <button
+          onClick={handleBack}
+          className="text-zinc-800 btn btn-ghost min-h-0 h-8 leading-tight mr-4"
+        >
           <FaArrowLeft className="h-6 w-6 text-zinc-800" />
         </button>
-        <button onClick={handleTrashClick} className='text-red-600 btn btn-ghost bg-min-h-0 h-8 leading-tight'>
+        <button
+          onClick={handleTrashClick}
+          className="text-red-600 btn btn-ghost bg-min-h-0 h-8 leading-tight"
+        >
           <FaTrash className="h-8 w-8 text-red-600" />
         </button>
       </div>
-      <div className='m-4 flex items-center'>
-        <h2 className="text-left text-4xl font-bold text-zinc-600">{section.title}</h2>
+      <div className="m-4 flex items-center">
+        <h2 className="text-left text-4xl font-bold text-zinc-600">
+          {section.title}
+        </h2>
       </div>
-      <h2 className='m-4 text-left text-2xl text-zinc-600 flex'>{section.data_type}</h2>       
-      <div className='m-4 flex'>{section.description}</div>
+      <h2 className="m-4 text-left text-2xl text-zinc-600 flex">
+        {section.data_type}
+      </h2>
+      <div className="m-4 flex">{section.description}</div>
 
-      <div className='m-4'>
-        <button onClick={handleAddAttributeClick} className='btn btn-primary my-4 text-white'>Add Attribute</button>
-        <h3 className="text-left text-xl font-semibold text-zinc-600">Attributes</h3>
-        <ul className='list-disc list-inside text-zinc-600'>
+      <div className="m-4">
+        <button
+          onClick={handleAddAttributeClick}
+          className="btn btn-primary my-4 text-white"
+        >
+          Add Attribute
+        </button>
+        <button
+          onClick={handleUpdateAttributeClick}
+          className="btn btn-primary ml-4 my-4 text-white"
+        >
+          Update Attributes
+        </button>
+        <h3 className="text-left text-xl font-semibold text-zinc-600">
+          Attributes
+        </h3>
+        <ul className="list-disc list-inside text-zinc-600">
           {Object.keys(attributes).map((key) => (
-            <li key={key} className='ml-4'>{key}</li>
+            <li key={key} className="ml-4">
+              {key}
+            </li>
           ))}
         </ul>
       </div>
       {isModalOpen && (
-        <DeleteSectionModal setIsModalOpen={setIsModalOpen} section={section} onBack={onBack} getDataSections={getDataSections}/>
+        <DeleteSectionModal
+          setIsModalOpen={setIsModalOpen}
+          section={section}
+          onBack={onBack}
+          getDataSections={getDataSections}
+        />
       )}
       {isAddAttributeModalOpen && (
-        <AddAttributeModal setIsAddAttributeModalOpen={setIsAddAttributeModalOpen} onBack={onBack} section={section} getDataSections={getDataSections}/>
+        <AttributeModal
+          setIsOpen={setIsAddAttributeModalOpen}
+          onBack={onBack}
+          section={section}
+          getDataSections={getDataSections}
+          mode="add"
+        />
+      )}
+      {isUpdateAttributeModalOpen && (
+        <AttributeModal
+          setIsOpen={setIsUpdateAttributeModalOpen}
+          onBack={onBack}
+          section={section}
+          getDataSections={getDataSections}
+          mode="edit"
+        />
       )}
     </div>
   );
