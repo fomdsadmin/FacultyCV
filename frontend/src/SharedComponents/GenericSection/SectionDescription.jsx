9c04f833ed6c7865db86ef5c4a3a7f31be6f160b
Xinyue Cao <ashleyue@students.cs.ubc.ca>
2025-07-03 14:09:05 -0700

    added show more button and new line

--- frontend/src/SharedComponents/GenericSection/SectionDescription.jsx
+++ frontend/src/SharedComponents/GenericSection/SectionDescription.jsx
@@ -1,9 +1,43 @@
 import { useGenericSection } from "./GenericSectionContext"
+import { useState } from "react";
+
+const MAX_LINES = 4;
 
 const SectionDescription = () => {
-    const { section } = useGenericSection()
+    const { section } = useGenericSection();
+    const [expanded, setExpanded] = useState(false);
 
-    return <div className="m-4 flex">{section.description}</div>
+    // Support both \newline and real newlines
+    const formattedDescription = section.description
+        ? section.description.trim().replace(/\/newline|\n/g, "<br>")
+        : "";
+
+    // Count lines for both cases
+    const lineCount = section.description
+        ? section.description.split(/\/newline|\n/).length
+        : 0;
+    return (
+        <div className="m-4 flex flex-col">
+            <div
+                className={`transition-all duration-200 overflow-hidden ${expanded ? "" : "line-clamp-3"}`}
+                style={
+                    !expanded
+                        ? { display: "-webkit-box", WebkitLineClamp: MAX_LINES, WebkitBoxOrient: "vertical" }
+                        : {}
+                }
+            >
+                <span dangerouslySetInnerHTML={{ __html: formattedDescription }} />
+            </div>
+            {section.description && section.description.length > 0 && lineCount > MAX_LINES && (
+                <button
+                    className="text-blue-600 text-sm mt-2 self-start hover:underline"
+                    onClick={() => setExpanded((prev) => !prev)}
+                >
+                    {expanded ? "Show less" : "Show more"}
+                </button>
+            )}
+        </div>
+    );
 }
 
 export default SectionDescription
