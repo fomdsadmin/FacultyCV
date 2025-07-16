fc34d43f5060d404005a98b0c8ec8a448d7533a0
Jake Yeo <JakeYeoZh@gmail.com>
2025-07-16 09:29:30 -0700

    Fixed create tables script.

--- cdk/lambda/createTables/createTables.py
+++ cdk/lambda/createTables/createTables.py
@@ -215,7 +215,7 @@ def lambda_handler(event, context):
     columns = []
     columns.append(createColumn('log_view_id', 'serial', 'PRIMARY KEY', False))
     columns.append(createColumn('ts', 'timestamp', '', False))
-    columns.append(createColumn('logged_user_email', 'text', 'NOT NULL', True))
+    columns.append(createColumn('logged_user_email', 'text', 'NOT NULL', False))
     columns.append(createColumn('logged_user_id', 'integer', 'NOT NULL', False))
     columns.append(createColumn('logged_user_first_name', 'text', 'NOT NULL', False))
     columns.append(createColumn('logged_user_last_name', 'text', 'NOT NULL', False))
@@ -226,7 +226,7 @@ def lambda_handler(event, context):
     columns.append(createColumn('page', 'text', '', False))
     columns.append(createColumn('session_id', 'text', '', False))
     columns.append(createColumn('assistant', 'boolean', '', False))
-    columns.append(createColumn('profile_record', 'text', '', False))
+    columns.append(createColumn('profile_record', 'text', '', True))
     query = createQuery('audit_view', columns)
     cursor.execute(query)
     
