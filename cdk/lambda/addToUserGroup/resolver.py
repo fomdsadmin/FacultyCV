9cd88ec589b8fa098544d2c3f7a8bd06edd998cb
Jake Yeo <JakeYeoZh@gmail.com>
2025-07-30 15:01:43 -0700

    Refactor addUserToGroup function to use username instead of sub_id for Cognito operations

--- cdk/lambda/addToUserGroup/resolver.py
+++ cdk/lambda/addToUserGroup/resolver.py
@@ -1,7 +1,7 @@
 import os
 import json
 import boto3
-
+ 
 def get_sub_by_email(user_pool_id, username):
     client = boto3.client('cognito-idp')
     response = client.list_users(
@@ -16,27 +16,30 @@ def get_sub_by_email(user_pool_id, username):
         if attr['Name'] == 'sub':
             return attr['Value']
     return None
-
+ 
 def addUserToGroup(arguments):
     user_name = arguments['userName']
     user_pool_id = os.environ['USER_POOL_ID']
     user_group = arguments['userGroup']
-
+ 
     sub_id = get_sub_by_email(user_pool_id, user_name)
-
+    print(sub_id)
+ 
     try:
         client = boto3.client('cognito-idp')
         response = client.admin_list_groups_for_user(
             UserPoolId=user_pool_id,
-            Username=sub_id
+            Username=user_name
         )
         groups = response['Groups']
+        print(groups)
         group_names = [g['GroupName'] for g in groups]
+        print(group_names)
         # Remove from 'NewlyRegistered' if present
         if 'NewlyRegistered' in group_names:
             client.admin_remove_user_from_group(
                 UserPoolId=user_pool_id,
-                Username=sub_id,
+                Username=user_name,
                 GroupName='NewlyRegistered'
             )
         # Check if user is already in the target group
@@ -53,6 +56,6 @@ def addUserToGroup(arguments):
             'statusCode': 500,
             'error': str(e)
         }
-
+ 
 def lambda_handler(event, context):
     return addUserToGroup(event['arguments'])
