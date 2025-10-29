3a71e77339afec436bccb2025a9e635fb7a1bd49
Jake Yeo <JakeYeoZh@gmail.com>
2025-10-29 13:22:25 -0700

    Fixed sort and filter date functions, and added missing grant table

--- frontend/src/Pages/ReportsPage/HtmlFunctions/FormatedData.js
+++ frontend/src/Pages/ReportsPage/HtmlFunctions/FormatedData.js
@@ -32,10 +32,10 @@ const filterDateRanges = (sectionData, dataSectionId) => {
 
     const section = sectionsMap[dataSectionId];
 
-    const startYear = Number(template.start_year);
-    const endYear = Number(template.end_year);
+    const templateStartYear = Number(template.start_year);
+    const templateEndYear = Number(template.end_year);
 
-    if (startYear === 0 || endYear === 0) {
+    if (templateStartYear === 0 || templateEndYear === 0) {
         return sectionData;
     }
 
@@ -70,16 +70,42 @@ const filterDateRanges = (sectionData, dataSectionId) => {
             return true;
         }
 
-        const cleaned = data["data_details"][dateAttribute].replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
-        const yearMatch = cleaned.match(/\d{4}/);
+        let startYear;
+        let endYear;
 
-        const year = yearMatch ? parseInt(yearMatch[0]) : null;
+        const [dirtyStartDate, dirtyEndDate] = data["data_details"][dateAttribute].split("-");
 
-        if (year === null) {
-            return true;
+        const currentYear = new Date().getFullYear();
+
+        if (dirtyStartDate) {
+            const cleanStartDate = dirtyStartDate.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+            const startYearMatch = cleanStartDate.match(/\d{4}/);
+            startYear = startYearMatch ? parseInt(startYearMatch[0]) : null;
+
+            if (cleanStartDate && cleanStartDate.toLowerCase() === "current") {
+                startYear = currentYear;
+            }
         }
 
-        return year >= startYear && year <= endYear;
+        if (dirtyEndDate) {
+            const cleanEndDate = dirtyEndDate.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+            const endYearMatch = cleanEndDate.match(/\d{4}/);
+            endYear = endYearMatch ? parseInt(endYearMatch[0]) : null;
+
+            if (cleanEndDate && cleanEndDate.toLowerCase() === "current") {
+                endYear = currentYear;
+            }
+        }
+
+        if (endYear) {
+            return endYear >= templateStartYear && endYear <= templateEndYear
+        }
+
+        if (startYear) {
+            return startYear >= templateStartYear && startYear <= templateEndYear
+        }
+
+        return true;
     });
 
     return sectionData;
@@ -110,6 +136,10 @@ const sortSectionData = (sectionData, dataSectionId) => {
     }
 
     return sectionData.sort((a, b) => {
+
+        const [aStartText, aEndText] = a["data_details"]?.[dateAttribute].split("-");
+        const [bStartText, bEndText] = b["data_details"]?.[dateAttribute].split("-");
+
         const cleanedA = a["data_details"]?.[dateAttribute]?.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
         const cleanedB = b["data_details"]?.[dateAttribute]?.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
 
@@ -119,10 +149,27 @@ const sortSectionData = (sectionData, dataSectionId) => {
         const yearMatchB = cleanedB?.match(/\d{4}/);
 
         // Handle null matches with fallbacks
-        const yearA = yearMatchA ? parseInt(yearMatchA[0]) : 0;
+        let yearA = yearMatchA ? parseInt(yearMatchA[0]) : null;
         const monthA = monthMatchA ? monthMatchA[0] : null;
-        const yearB = yearMatchB ? parseInt(yearMatchB[0]) : 0;
+        let yearB = yearMatchB ? parseInt(yearMatchB[0]) : null;
         const monthB = monthMatchB ? monthMatchB[0] : null;
+
+        const currentYear = new Date().getFullYear();
+        if (!yearA) {
+            if (aStartText?.toLowerCase() === "current" || aEndText?.toLowerCase() === "current") {
+                yearA = currentYear;
+            } else {
+                yearA = -Infinity;
+            }
+        }
+
+        if (!yearB) {
+            if (bStartText?.toLowerCase() === "current" || bEndText?.toLowerCase() === "current") {
+                yearB = currentYear
+            } else {
+                yearB = -Infinity;
+            }
+        }
 
         // Sort by year
         const yearComparison = yearA - yearB;
@@ -730,7 +777,44 @@ const buildSubSections = (preparedSectionWithSubSections) => {
         tables = separateIntoRefereedAndNonRefereed(tables, preparedSectionWithSubSections);
     }
 
+    if (preparedSectionWithSubSections.title === "9[b-c]. Research or Equivalent Grants and Contracts") {
+        const underReviewResearchGrantsTable = buildUnderReviewResearchGrants(tables);
+
+        if (tables[0]) {
+            tables.splice(1, 0, underReviewResearchGrantsTable);
+        }
+    }
+
     return tables;
+}
+
+const buildUnderReviewResearchGrants = (tables) => {
+
+    if (tables.length === 0) {
+        return tables;
+    }
+
+    const underReviewResearchGrantsTable = structuredClone(tables[0]);
+
+    const rows = tables
+        .flatMap((table) => table.rows)
+        .filter((row) => row["Status - Only for Grants"] === "Under Review");
+
+    underReviewResearchGrantsTable.rows = rows;
+
+    let updatedHeaderName = "Grants Submitted and Currently Under Review Research Grants";
+    if (underReviewResearchGrantsTable?.columns?.[0]) {
+
+        const headerName = underReviewResearchGrantsTable.columns[0].headerName;
+
+        if (headerName.includes("(") && headerName.includes(")")) {
+            updatedHeaderName += ` (${rows.length})`;
+        }
+
+        underReviewResearchGrantsTable.columns[0].headerName = updatedHeaderName;
+    }
+
+    return underReviewResearchGrantsTable;
 }
 
 const separateIntoRefereedAndNonRefereed = (tables, preparedSection) => {
